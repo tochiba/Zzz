ad2d0c7711b684625ab16f4e7f2a031950b3fba2
tochiba <c104kn@gmail.com>
2016-04-23 01:29:49 +0900

    first commit

---
+++ Zzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zzz
+//
+//  Created by 千葉 俊輝 on 2016/04/23.
+//  Copyright © 2016年 Toshiki Chiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
